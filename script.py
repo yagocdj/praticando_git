ff45f1fd1ddcdf2dec2785442c8ecb84281968f1
Yago César <yagocdj@gmail.com>
2023-10-13 20:51:54 -0300

    add oi.txt and script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+# count from one to ten with a sleep of 1 second between each step
+print('Starting count ...')
+
+for i in range(10):
+    print(i)
+    sleep(1)
+
+print('Done!')
+
