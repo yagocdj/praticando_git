ca6ffe738c8b013dc1c3014a0d3f9d33af9ec24e
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-10-19 16:26:59 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 # count from one to ten with a sleep of 1 second between each step
-print('Starting count ...')
+print('Pablo Estrela')
 
 for i in range(10):
     print(i)
