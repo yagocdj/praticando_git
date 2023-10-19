39e76caf933a23fe46b52bd6c1615c173895203d
Yago César <yagocdj@gmail.com>
2023-10-19 20:49:45 -0300

    modificando o script.py com o meu nome e tirando um comentário dele

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
 from time import sleep
 
 # count from one to ten with a sleep of 1 second between each step
-print('PabloEstrela')
+print('Yago César')
 
 for i in range(10):
     print(i)
     sleep(1)
-#AMEGAN
+
 print('Done!')
 
